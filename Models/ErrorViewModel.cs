42348990034b292837a72d2854d08f9093de1960
Carlos Henrique <carloshmde.oliveira@gmail.com>
2025-07-19 09:36:51 -0300

    Commit inicial: estrutura base do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaEnsinE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
